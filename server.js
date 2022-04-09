7b0b8ec9002e38202235f76b6b1b2d04701aaeab
Creator <382988057@qq.com>
2022-04-09 20:07:18 +0800

    dd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-27 14:10:51
- * @LastEditTime: 2022-04-09 20:04:36
+ * @LastEditTime: 2022-04-09 20:06:51
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /nginx-docker/server.js
@@ -9,7 +9,7 @@
 const http = require('http');
 
 let users = [{
-    id: 11, name: '13331mark----'
+    id: 11, name: '444mark----'
 }, {
     id: 2, name: 'Creator'
 }]
